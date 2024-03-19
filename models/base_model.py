9af60c7b784383ef539fcbd83fd0576001aefccd
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-03-19 21:40:02 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,30 @@
 """Define BaseModel Class."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+    def __init__(self, *args, **kwargs):
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")) #String to datetime object
+                    
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+       
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
