75f93651709bf5034879c3df7004de8def05846c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-03-20 21:32:06 +0300

    Console 0.01

---
+++ console.py
@@ -0,0 +1,18 @@
+"""Entry point of the command interpreter."""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command line interpreter for the AirBnB clone."""
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """Implement EOF."""
+        return True
+    
+    def do_quit(self, *args):
+        """Exit the program."""
+        return True
+    
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
