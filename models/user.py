2da41b46023f8ef3399d0598d778f797203b694b
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-03-21 17:21:05 +0300

    Add class User and Modify FileStorage

---
+++ models/user.py
@@ -0,0 +1,11 @@
+"""Class user inherits from BaseModel Class."""
+
+from base_model import BaseModel
+
+class User(BaseModel):
+    """User inherits from BaseModel class."""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
