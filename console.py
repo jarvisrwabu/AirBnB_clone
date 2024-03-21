10b52ce134bc196005c6e270b96b72d04e361a40
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-03-21 16:24:27 +0300

    Implement destroy command

--- console.py
+++ console.py
@@ -19,7 +19,11 @@ class HBNBCommand(cmd.Cmd):
     valid_classes = ["BaseModel"]
 
     def do_create(self, model):
-        """Create a new instance of BaseModel, saves it (to the JSON file) and prints the id."""
+        """
+        Create a new instance of BaseModel, saves it (to the JSON file) and prints the id.
+        Usage: create <BaseModel>
+        
+        """
         input_cmd = shlex.split(model)
         if len(input_cmd) == 0:
             print("** class name missing **")
@@ -34,7 +38,11 @@ class HBNBCommand(cmd.Cmd):
             print(new_model.id)
             
     def do_show(self, str_rep):
-        """Print the string representation of an instance based on the class name and id."""
+        """
+        Print the string representation of an instance based on the class name and id.
+        Usage: show <class_name> <id>
+        
+        """
         input_cmd = shlex.split(str_rep)
         if len(input_cmd) == 0:
             print("** class name missing **")
@@ -56,11 +64,37 @@ class HBNBCommand(cmd.Cmd):
                     instance_data = data[instance_key]
                     print(f"[{class_name}] ({instance_id}) {instance_data}")
                      
+                else:
+                    print("** no instance found **")
                      
-    def do_destroy(self, line):
-        """Deletes an instance based on the class name and id (save the change into the JSON file)."""
-        pass
+    def do_destroy(self, dest_arg):
+        """
+        Deletes an instance based on the class name and id (save the change into the JSON file).
+        Usage: destroy <class_name> <id>
         
+        """
+        input_cmd = shlex.split(dest_arg)
+        
+        if len(input_cmd) == 0:
+            print("** class name missing **")
+            
+        elif input_cmd[0] not in self.valid_classes:
+            print("** class doesn't exist **")    
+            
+        elif len(input_cmd) < 2:
+            print("** instance id missing **")
+          
+        else:
+            objects = storage.all()
+            instance_key = "{}.{}".format(input_cmd[0], input_cmd[1]) # Class name and id
+            
+            if instance_key in objects:
+                del objects[instance_key]
+                storage.save()
+            
+            else:
+                print("** no instance found **")
+            
                          
     def do_EOF(self, line):
         """Implement EOF."""
