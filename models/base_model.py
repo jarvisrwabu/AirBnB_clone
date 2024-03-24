cab2c03802ea009a817b914505b695c5617bbf5c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-03-24 11:45:59 +0300

    Solve Pycodestyle and pydocstyle issues in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,16 @@ import models
 
 
 class BaseModel:
+    """Define Template for other classes."""
+
     def __init__(self, *args, **kwargs):
+        """Give attributes once instance is created."""
         if (kwargs):
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")) #String to datetime object
-                    
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+
                 elif key != '__class__':
                     setattr(self, key, value)
         else:
@@ -19,15 +23,18 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-       
+
     def __str__(self):
+        """Return string object."""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Save Object."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """Convert to dict object."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
